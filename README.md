6d098b5b5757b792d6a27f8aa1ef0fb88a00fbb9
Niko <Vinicius@email.com>
2025-06-06 20:14:14 -0300

    Um Fork do codigo do linux

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-"# Bora-la-ne-cpp-windows" 
+# bora-lá-né-cpp
+## Esse é meu repositorio no github so para eu estudar e compartilhar
+### Nesse repositorio eu vou usar:
+1. **C++**
+    - \#eu SOU um **iniciante** AINDA
+2. **Cmake**
+    - \#vou aprender no ***futuro***
+3. **EditorConfig**
+    - \#vou aprender no ***futuro***. \#Eu vou usar ele para **padronizar meus codigos**. Ache bem enteresante.
+4. **Clang++** ou **G++**.
+    - \#estou ainda aprendendo a **utilizar** esse compiladores
+5. **DrawIo**.
+    - \#eu to basicamento fazendo o *diagrama* do git mais ele **server muito mais que isso**.
+6. **Bibliotecas(.H)**
+    - \#Porque eu queria eu quero muito saber como que utiliza e como criar.
+7. **Bibliotecas(.DLL)**
+    - \#So para fazer bibliotecas dinamicas.
+8. **Linux**
+    - \#eu utilizo o **LUBUNTU** "então não sei se eu compilar codigo no      linux eu ***não sei se Você vai conseguir executar no WINDOWS***".
+9. **MakeFile**
+    - \#Ele usa para altomatizar a compilação. Parece **Interesnate**.
+---
+## Agora aqui é o link de um documento HTML **que ainda estou fazendo**
+### Esse é o site [teste](html/site-de-test/index.html)
+---
+![Imagens](niko.gif)
+#### Para abrir o editor Markdown
+##### 1. **Ctrl+Shift+V**
---
+++ aulas/aula4/aula4.cpp
@@ -0,0 +1,37 @@
+//variaveis e input do teclado
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //tops de Variaveis heeepyyyyy
+    //essa partes eu ja sei mas estou recomeçando o estudo para verse não perdi nada
+    int inteiros;
+    float ponto;
+    double pontos;
+    bool VOrF;
+    char s;
+    string sal;
+    char array[11]{};
+    //e nessa parte quando você ver um array é porque eu ja estudei outras linguagens ate o mediano tipi C#
+    //agora tira mão desse teclado!!
+    cout << "dijite numero(s) inteiros para colocar na variavel\n";
+    cin >> inteiros;
+    cout << "djite um numero com ponto tipo:4.32\n";
+    cin >> ponto;
+    cout << "dijite um numero com mais duas casas de pois do ponto\n";
+    cin >> pontos;
+    cout << "Dijite um ou zero\n";
+    cin >> VOrF;
+    cout << "Dijite APENAS uma letra\n";
+    cin >> s;
+    cout << "Dijite qualquer coisa com mais de uma letra\n";
+    cin >> sal;
+    cout << "Dijite mais de uma vez qualquer coisa com MENOS de 10 caracteres\n";
+    cin >> array;
+    cout << "Esse são o valores que você atribuiu as variaveis\n";
+    cout << inteiros << "\n" << ponto  << "\n" << pontos << "\n" << VOrF << "\n" << s << "\n" << sal << "\n" << array << endl;
+    cout << "Precione a tecla ENTER para sair do programa" << endl;
+    system("pause");
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
+}
